68b3b2668b7804789fbf10ae3b6f65ba5374e08c
Tobias Hochgürtel <tobias.hochguertel@googlemail.com>
2026-09-09 21:58:17 +0200

    test: fix stream-reader URL

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
 		res.append(.package(url: "https://github.com/apple/swift-system.git",                  from: "1.0.0")) /* We’re aware of the existence of System on macOS. After some thinking/research, we decided to agree with <https://forums.swift.org/t/50719/5>. */
 		res.append(.package(url: "https://github.com/Frizlab/UnwrapOrThrow.git",               from: "1.0.1"))
 		res.append(.package(url: "https://github.com/xcode-actions/clt-logger.git",            from: "1.0.0-beta.4"))
-		res.append(.package(url: "https://github.com/xcode-actions/stream-reader.git",         from: "3.6.0"))
+		res.append(.package(url: "https://github.com/Frizlab/stream-reader.git",         from: "3.6.0"))
 		res.append(.package(url: "https://github.com/tobiashochguertel/swift-signal-handling.git", .upToNextMinor(from: "1.1.2")))
 		if useXtenderZ {
 			res.append(.package(url: "https://github.com/Frizlab/eXtenderZ.git",                from: "2.0.0"))
